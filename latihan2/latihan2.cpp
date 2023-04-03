9e1ecf74731e6971e1a2579fafb50a28bf376634
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-04-03 11:01:46 +0700

    menjalankan program

--- latihan2/latihan2.cpp
+++ latihan2/latihan2.cpp
@@ -7,10 +7,32 @@ struct AlamatDetail
 	string kota;
 };
 
-struct Mahasiwa
+struct Mahasiswa
 {
 	string NIM;
 	string nama;
-	string alamat;
+	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+	cout << "masukan NIM :";
+	cin >> mhs.NIM;
+	cout << "masukan Nama :";
+	cin >> mhs.nama;
+	cout << "masukan Alamat :";
+	cout << "\n\tmasukan Desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukan Kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "masukan umur : ";
+	cin >> mhs.umur;
+	 
+	cout << "\n\nNIM = " << mhs.NIM;
+	cout << "\nNama = " << mhs.nama;
+	cout << "\nDesa = " << mhs.alamat.desa;
+	cout << "\nkota = " << mhs.alamat.kota;
+	cout << "\nUmur = " << mhs.umur;
+
+}
