f7bca89d4e42f07d08ce1e959003b5576a88395b
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-04-03 10:45:40 +0700

    masukan sturuktur mahasiswa

---
+++ latihan2/latihan2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct AlamatDetail
+{
+	string desa;
+	string kota;
+};
+
+struct Mahasiwa
+{
+	string NIM;
+	string nama;
+	string alamat;
+	int umur;
+};
