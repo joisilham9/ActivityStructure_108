dc644e7fa5e056e840fcecf19047674322b8772a
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-04-03 10:24:33 +0700

    memasukan isi data atribut mhs1

--- ActivityStructure_108/ActivityStructure_108.cpp
+++ ActivityStructure_108/ActivityStructure_108.cpp
@@ -4,7 +4,30 @@ using namespace std;
 struct mahasiswa
 {
 	string NIM;
-	string ama;
+	string nama;
 	string alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20220140108";
+	mhs1.nama = "jois ilham Novaldi";
+	mhs1.alamat = "sidorejo";
+	mhs1.NIM = "19";
+
+	cout << "masukan NIM";
+	cin >> mhs2.NIM;
+	cout << "masukan Nama";
+	cin >> mhs2.nama;
+	cout << "masukan alamat";
+	cin >> mhs2.alamat;
+	cout << "masukan umur";
+	cin >> mhs2.umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUmur = " << mhs1.umur;
+}
